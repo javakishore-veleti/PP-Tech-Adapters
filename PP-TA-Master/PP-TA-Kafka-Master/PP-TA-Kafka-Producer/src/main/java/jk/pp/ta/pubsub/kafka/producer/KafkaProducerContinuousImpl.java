8858ec4fcb4273f61793dd8a5ffcdc01e572d7c3
KISHORE VELETI <vavkkishore@kishores-mbp.home>
2021-05-24 14:07:58 -0400

    Initial Commit of PP-Tech-Adapters

--- PP-TA-Master/PP-TA-Kafka-Master/PP-TA-Kafka-Producer/src/main/java/jk/pp/ta/pubsub/kafka/producer/KafkaProducerContinuousImpl.java
+++ PP-TA-Master/PP-TA-Kafka-Master/PP-TA-Kafka-Producer/src/main/java/jk/pp/ta/pubsub/kafka/producer/KafkaProducerContinuousImpl.java
@@ -1,27 +1,44 @@
 package jk.pp.ta.pubsub.kafka.producer;
 
+import java.util.List;
+
+import org.javatuples.Pair;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
+import jk.pp.engg.foundations.common.core.pubsub.PubSubCallBackHandler;
 import jk.pp.engg.foundations.common.core.pubsub.PubSubKey;
 import jk.pp.engg.foundations.common.core.pubsub.PubSubMessage;
 import jk.pp.engg.foundations.common.core.pubsub.PubSubResult;
-import jk.pp.engg.foundations.common.pubsub.AppPubSubPublisher;
-import jk.pp.engg.foundations.common.pubsub.PubSubCallBackHandler;
+import jk.pp.engg.foundations.common.core.pubsub.PubSubTopic;
+import jk.pp.engg.foundations.common.service.core.pubsub.PubSubProducerService;
 
 @ConditionalOnProperty(name = "pp.ta.pubsub.kafka.producer.apache.enabled", havingValue = "true")
 @Component("KafkaProducerContinuousImpl")
-public class KafkaProducerContinuousImpl implements AppPubSubPublisher<PubSubKey, PubSubMessage, PubSubResult> {
+public class KafkaProducerContinuousImpl implements PubSubProducerService<PubSubKey, PubSubMessage, PubSubResult> {
 
 	@Override
-	public PubSubResult publish(String topicName, PubSubKey key, PubSubMessage message,
-			PubSubCallBackHandler<PubSubKey, PubSubMessage, PubSubResult> callBackHandler) throws Exception {
+	public PubSubResult publishMessage(String topic, Pair<PubSubKey, PubSubMessage> msgKeyAndVal,
+			PubSubTopic pubSubTopic, PubSubCallBackHandler<PubSubKey, PubSubMessage, PubSubResult> callBackHandler) throws Exception {
 
 		PubSubResult result = new PubSubResult();
 		if (callBackHandler != null) {
-			callBackHandler.callback(key, message, result);
+			callBackHandler.callback(msgKeyAndVal, result);
 		}
-		return new PubSubResult();
+
+		return result;
+	}
+
+	@Override
+	public PubSubResult publishMessages(String topic, List<Pair<PubSubKey, PubSubMessage>> msgKeyAndVals,
+			PubSubTopic pubSubTopic, PubSubCallBackHandler<PubSubKey, PubSubMessage, PubSubResult> callBackHandler) throws Exception {
+
+		PubSubResult result = new PubSubResult();
+		if (callBackHandler != null) {
+			callBackHandler.callback(msgKeyAndVals, result);
+		}
+
+		return result;
 	}
 
 }
